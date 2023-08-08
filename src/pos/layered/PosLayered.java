deb07ad66f6eae748cc3693cb693892458156e6c
sachinthadilshan <sachinthadilshan96@gmail.com>
2023-08-08 17:46:16 +0530

    layout,customer views done

--- src/pos/layered/PosLayered.java
+++ src/pos/layered/PosLayered.java
@@ -4,6 +4,8 @@
  */
 package pos.layered;
 
+import pos.layered.view.LayoutView;
+
 /**
  *
  * @author sachinthadilshan
@@ -15,6 +17,7 @@ public class PosLayered {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        new LayoutView().setVisible(true);
     }
     
 }
